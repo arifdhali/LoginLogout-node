f11d9c8a97baa3e50ce45ef575a5cba345a10db0
Arif <webgrity161@gmail.com>
2024-02-29 18:37:58 +0530

    trying to use session

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const connection = require('./db/db-config');
+const session = require('express-session');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,48 +11,29 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('pages/login');
-});
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}))
 
 
-app.get('/dashboard', (req, res) => {
-    res.render('pages/dashboard');
-});
+app.get("/", (req, res) => {
+    res.render("pages/index", [{ title: "Login page" }]);
 
 
-app.post('/login', (req, res) => {
+})
+app.post("/login", (req, res) => {
     const { email, password } = req.body;
-
-    connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [email, password], (err, results) => {
-        if (err) {
-            console.error('Error querying database: ' + err.stack);
-            res.send('An error occurred');
-            return;
-        }
-
-        if (results.length > 0) {
-            res.redirect("/dashboard");
-        } else {
-            res.render('pages/login', results);
-        }
-    });
-});
+    console.log(email, password);
+    res.render("pages/index", [{ title: "Login page" }]);
 
 
-app.post('/create', (req, res) => {
-    const { username, email, password } = req.body;
-    let insertSQL = 'INSERT INTO users (username,email,password) VALUES(?,?,?)';
-    connection.query(insertSQL, [username, email, password], (err, result) => {
+})
 
-        if (err) {
-            console.log("Error inserting");
-        } else {
-            res.redirect("dashboard");
-        }
 
-    });
-});
+
+
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
